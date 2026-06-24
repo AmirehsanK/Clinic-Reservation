b1d9630f18a70c81a63809f9536475074b4cc3d1
Darkerys <amirehsank3@gmail.com>
2026-06-24 14:02:55 +0330

    migrations and bugs

---
+++ Clinic.Data/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Clinic.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clinic.Data.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {}
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<ReserveRecord> ReserveRecords { get; set; }
+}
