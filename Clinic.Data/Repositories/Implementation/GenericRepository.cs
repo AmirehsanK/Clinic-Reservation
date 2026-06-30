b50b9e480af298f387adfb0339eb46b873beebf4
Darkerys <amirehsank3@gmail.com>
2026-06-30 16:07:37 +0330

    GenericRepository Interface

---
+++ Clinic.Data/Repositories/Implementation/GenericRepository.cs
@@ -0,0 +1,58 @@
+﻿using Clinic.Data.Entities;
+using Clinic.Data.Repositories.Interface;
+
+namespace Clinic.Data.Repositories.Implementation;
+
+public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : BaseEntity
+{
+    
+
+    public Task<TEntity> GetEntityById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<TEntity> GetAllEntities()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Create(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task CreateRangeEntities(List<TEntity> entities)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Delete(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteRange(List<TEntity> entities)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeletePermanently(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SaveChanges()
+    {
+        throw new NotImplementedException();
+    }
+    public ValueTask DisposeAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Clinic.Data/Repositories/Interface/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using Clinic.Data.Entities;
+
+namespace Clinic.Data.Repositories.Interface;
+
+public interface IGenericRepository<TEntity> : IAsyncDisposable where TEntity : BaseEntity
+{
+    Task<TEntity> GetEntityById(int id);
+    
+    IQueryable<TEntity> GetAllEntities();
+
+    Task Create(TEntity entity); 
+    
+    Task CreateRangeEntities(List<TEntity> entities);
+    
+    Task Delete(TEntity entity);
+    
+    Task DeleteRange(List<TEntity> entities);
+    
+    void Update(TEntity entity);
+    
+    void DeletePermanently(int id);
+    
+    Task SaveChanges();
+}
