86d38e8de10ec7354a120ffedbe1c11b570ffc40
Darkerys <amirehsank3@gmail.com>
2026-07-09 22:17:05 +0330

    Record P3

--- Clinic.Application/Services/Implementation/RecordService.cs
+++ Clinic.Application/Services/Implementation/RecordService.cs
@@ -4,5 +4,8 @@ namespace Clinic.Application.Services.Implementation;
 
 public class RecordService : IRecordService
 {
-    
+    public async ValueTask DisposeAsync()
+    {
+        throw new NotImplementedException();
+    }
 }
