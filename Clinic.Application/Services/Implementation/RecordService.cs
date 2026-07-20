17e96e339070cd20212375eacca67b1d585822c3
Darkerys <amirehsank3@gmail.com>
2026-07-20 14:13:19 +0330

    p6

--- Clinic.Application/Services/Implementation/RecordService.cs
+++ Clinic.Application/Services/Implementation/RecordService.cs
@@ -5,7 +5,7 @@ using Clinic.Application.Services.Interfaces;
 namespace Clinic.Application.Services.Implementation;
 
 public class RecordService : IRecordService
-{
+{ 
     public async Task<FilterRecordsDto> FilterRecords(FilterRecordsDto filter)
     {
         throw new NotImplementedException();
