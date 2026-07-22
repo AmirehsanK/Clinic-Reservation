4768f39ac63a0c56ea6e4de5147793cad5d8f557
Darkerys <amirehsank3@gmail.com>
2026-07-22 11:20:13 +0330

    Controllers

---
+++ Clinic.Mvc/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clinic.Mvc.Controllers;
+
+public class AccountController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Clinic.Mvc/Controllers/ReservationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clinic.Mvc.Controllers;
+
+public class ReservationController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
