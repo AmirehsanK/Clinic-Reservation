7981828a68657dba557068bc32d62a16d784e180
Darkerys <amirehsank3@gmail.com>
2026-07-24 19:32:52 +0330

    P1

--- Clinic.Application/Services/Implementation/OtpService.cs
+++ Clinic.Application/Services/Implementation/OtpService.cs
@@ -1,6 +1,26 @@
-﻿namespace Clinic.Application.Services.Implementation;
+﻿using Clinic.Application.Services.Interfaces;
 
-public class OtpService
+namespace Clinic.Application.Services.Implementation;
+
+public class OtpService :IOtpService
 {
-    
+    public async ValueTask DisposeAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void GenerateOtp(string mobile)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool ValidateOtp(string mobile, string otp)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool ResendOtp(string mobile)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Clinic.Application/Services/Interfaces/IOtpService.cs
+++ Clinic.Application/Services/Interfaces/IOtpService.cs
@@ -1,6 +1,8 @@
 ﻿namespace Clinic.Application.Services.Interfaces;
 
-public interface IOtpService
+public interface IOtpService : IAsyncDisposable
 {
-    
+    void GenerateOtp(string mobile);
+    bool ValidateOtp(string mobile, string otp);
+    bool ResendOtp(string mobile);
 }
