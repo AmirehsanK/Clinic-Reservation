e55e51ca49e9185ec5bd508131229a000b0ce433
Darkerys <amirehsank3@gmail.com>
2026-08-03 21:40:28 +0330

    -

--- Clinic.Mvc/Controllers/AdminController.cs
+++ Clinic.Mvc/Controllers/AdminController.cs
@@ -46,7 +46,6 @@ public class AdminController(IUserService userService) : BaseController
         }
         TempData[ErrorMessage] = res.Message;
         return View(dto);
-        
     }
     
     #endregion
